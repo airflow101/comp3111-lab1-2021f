8af21f617c6a49e8e234fce33caf64278720aa6b
bryan <bryan@DESKTOP-4DATI14>
2021-09-22 23:28:42 +0800

    Method call and fix typo

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -6,8 +6,9 @@ package Lab1;
 public class Library {
 	
 	public static void main(String arg[]) {
-		System.out.println("Hellow, World!");
-		
+		Library lib = new Library();
+		if (lib.someLibraryMethod())
+			System.out.println("Hello, World!");
 	}
     public boolean someLibraryMethod() {
         return true;
